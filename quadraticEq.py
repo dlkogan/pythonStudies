4f11000b22433eafff0c1924e01eeea29d66a232
Danielle Kogan <kogandanielle@gmail.com>
2020-05-19 21:13:29 -0400

    baby's first python programs

---
+++ quadraticEq.py
@@ -0,0 +1,7 @@
+# this helps us with the quadratic equation
+#hi!! :)
+print "Hello! Need help with the quadratic equation?"
+print "y = ax^2 +bx + c"
+a,b,c = input("type the coefficients a,b,and c separating by commas.")
+x = input("input 'x' to solve for y!")
+print "y is equal to", (a * (x * x)) + (b * x) + c
---
+++ tempConv.py
@@ -0,0 +1,7 @@
+# tempConv.py
+#A fun temp converter
+print "hi, time to convert some numbies!"
+print "to farenheit!"
+c = input("What celcius temp would you like to convert?")
+f = c * 9.0/5.0 +32
+print "The equivalent temp in F is", f
